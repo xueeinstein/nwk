e9d98401c2452499e7291b3845354104675f2a7a
xueeinstein <xueeinstein@gmail.com>
2015-05-12 07:57:27 +0800

    add install test cases

--- test/installSpec.js
+++ test/installSpec.js
@@ -1,9 +1,19 @@
 var expect = require("chai").expect;
+var install = require("../lib/install.js");
 
-decribe("install", function () {
-  decribe("#check", function () {
-    it("check pure js package", function() {
-      
+describe("install", function () {
+  describe("#checkInstallType()", function () {
+    it("check pure js package, taking 'colors' as an example", function(done) {
+      var type = install.checkInstallType("colors");
+      expect(type).to.equal("PUREJS");
+    });
+    it("check addone package which using 'node-gyp' to install, taking 'bignum' as an example", function(done) {
+      var type = install.checkInstallType("bignum");
+      expect(type).to.equal("ADDONS_I");
+    });
+    it("check addone package which using 'node-pre-gyp' to install, taking 'sqlite3' as an example", function(done) {
+      var type = install.checkInstallType("sqlite3");
+      expect(type).to.equal("ADDONS_II");
     });
   });
 });
