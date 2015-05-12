5cfe4b21c8db3f29fda671d668c3ecb3243274df
xueeinstein <xueeinstein@gmail.com>
2015-05-13 07:50:37 +0800

    support singlePkg install

--- nn.js
+++ nn.js
@@ -1,3 +1,4 @@
+// Ignore this file, just for node debug easily!
 var npm = require("npm");
 var nopt = require("nopt");
 
@@ -6,18 +7,37 @@ var parsed = nopt(confDefs.types, confDefs.shorthands);
 
 var command = parsed.argv.remain.shift();
 
-var nodedir = require("./lib/nodedir.js");
-var fs = require("fs");
-var path = require("path");
-var errorHandler = require("./lib/utils/error-handler.js");
-var config = require("./config.js");
-nodedir.cache("0.8.3", function (er) {
-	if (er) errorHandler(er);
-	var isCached = fs.existsSync(path.resolve(config.cacheDir, "0.8.3"));
-	console.log(isCached);
-});
-//console.log(parsed);
+//var nodedir = require("./lib/nodedir.js");
+//var fs = require("fs");
+//var path = require("path");
+//var errorHandler = require("./lib/utils/error-handler.js");
+//var config = require("./config.js");
+//nodedir.cache("0.12.0", function (er) {
+	//if (er) errorHandler(er);
+	//var isCached = fs.existsSync(path.resolve(config.cacheDir, "0.8.3"));
+	//console.log(isCached);
+//});
 
+var phonyConf = require("./lib/config/phonyConf.js");
+
+var conf = phonyConf.view("colors", "scripts.install");
+//console.log(conf);
+//npm.load(conf, function (er) {
+  //debugger
+  //if (er) return errorHandler(er);
+  //npm.commands.view(["colors", "scripts"], function(er, data){
+    //if (er) return errorHandler(er); // npm default handler
+    //if (data) {
+      //console.log(data);
+      //return "PUREJS";
+    //}
+  //});
+//});
+console.log(parsed);
+var install = require("./lib/install.js");
+debugger
+var type = install.checkInstallType("colors");
+console.log(type);
 //npm.load(parsed, function (argument) {
   ////var list = npm.config.get("list");
   ////console.log(npm.config);
--- test/installSpec.js
+++ test/installSpec.js
@@ -1,19 +1,88 @@
 var expect = require("chai").expect;
-var install = require("../lib/install.js");
+var path = require("path"),
+    nwk = require("../lib/nwk.js"),
+    install = require("../lib/install.js"),
+    phonyConf = require("../lib/config/phonyConf.js");
 
 describe("install", function () {
   describe("#checkInstallType()", function () {
     it("check pure js package, taking 'colors' as an example", function(done) {
-      var type = install.checkInstallType("colors");
-      expect(type).to.equal("PUREJS");
+      this.timeout(150000);
+      install.checkInstallType("colors", function (er, res) {
+        if (er) throw er;
+        expect(res).to.equal("PUREJS");
+        done();
+      });
     });
     it("check addone package which using 'node-gyp' to install, taking 'bignum' as an example", function(done) {
-      var type = install.checkInstallType("bignum");
-      expect(type).to.equal("ADDONS_I");
+      this.timeout(150000);
+      install.checkInstallType("bignum", function (er, res) {
+        if (er) throw er;
+        expect(res).to.equal("ADDONS_I");
+        done();
+      });
     });
     it("check addone package which using 'node-pre-gyp' to install, taking 'sqlite3' as an example", function(done) {
-      var type = install.checkInstallType("sqlite3");
-      expect(type).to.equal("ADDONS_II");
+      this.timeout(150000);
+      install.checkInstallType("sqlite3", function (er, res) {
+        if (er) throw er;
+        expect(res).to.equal("ADDONS_II");
+        done();
+      });
+    });
+  });
+
+  describe("#singlePkg()", function () {
+    it("install single pure js package 'colors' for nw.js-v0.12.0, under tmp dir", function(done) {
+      this.timeout(150000);
+      var conf = phonyConf.install("colors", "0.12.0"),
+          tmp = path.resolve(__dirname, "..", "nwktmp");
+      process.chdir(tmp);
+      console.log(__dirname);
+      console.log(process.cwd());
+      install.singlePkg(conf, function (er, res) {
+        if (er) {
+          process.chdir(__dirname);
+          throw er;
+        }
+        expect(res).to.equal("ok");
+        process.chdir(__dirname);
+        done();
+      });
+    });
+    it("install ADDONS_I package 'bignum' for nw.js-v0.12.0, under tmp dir", function(done) {
+      this.timeout(150000);
+      var conf = phonyConf.install("bignum", "0.12.0"),
+          tmp = path.resolve(__dirname, "..", "nwktmp");
+      process.chdir(tmp);
+      console.log(__dirname);
+      console.log(process.cwd());
+      install.singlePkg(conf, function (er, res) {
+        if (er) {
+          process.chdir(__dirname);
+          throw er;
+        }
+        expect(res).to.equal("ok");
+        process.chdir(__dirname);
+        done();
+      });
+    });
+    it("install ADDONS_II package 'sqlite3' for nw.js-v0.12.0, under tmp dir", function(done) {
+      this.timeout(150000);
+      var conf = phonyConf.install("sqlite3", "0.12.0"),
+          tmp = path.resolve(__dirname, "..", "nwktmp");
+      process.chdir(tmp);
+      console.log(__dirname);
+      console.log(process.cwd());
+      install.singlePkg(conf, function (er, res) {
+        if (er) {
+          process.chdir(__dirname);
+          throw er;
+        }
+        expect(res).to.equal("ok");
+        process.chdir(__dirname);
+        done();
+      });
     });
   });
 });
